afbb002d2e4038b6fa5318bf25ad5ae628d2db9d
angelorange <acostalarangeira@gmail.com>
2025-09-27 17:32:57 -0300

    add dependencies

--- mix.exs
+++ mix.exs
@@ -20,9 +20,12 @@ defmodule Bitpack.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-    ]
+  [
+    {:ex_doc, "~> 0.32", only: :dev, runtime: false},
+    {:stream_data, "~> 0.6", only: :test},
+    {:benchee, "~> 1.3", only: :dev},
+    {:dialyxir, "~> 1.4", only: :dev, runtime: false},
+    {:jason, "~> 1.4", only: [:dev, :test]}
+  ]
   end
 end
