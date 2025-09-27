05d70baf2e6a04cbaec473a37dc2175f1ff5b5cc
angelorange <acostalarangeira@gmail.com>
2025-09-27 12:26:32 -0300

    first commit

---
+++ lib/bitpack.ex
@@ -0,0 +1,18 @@
+defmodule Bitpack do
+  @moduledoc """
+  Documentation for `Bitpack`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Bitpack.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
